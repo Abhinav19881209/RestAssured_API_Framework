7cd721c5f813cb2ab963f64aaba55f54a99f6e4b
Abhinav19881209 <abhinav.khadatkar20@gmail.com>
2023-08-01 21:59:25 +0530

    Chnages in Demo Class

---
+++ src/main/java/DemoGit/DemoClass.java
@@ -0,0 +1,14 @@
+package DemoGit;
+
+public class DemoClass {
+	
+	public void Method1() {
+		
+		System.out.println("this for Giit demo");
+	}
+	
+public void Method2() {
+		
+		System.out.println("this for Giit demo");
+	}
+}
---
+++ src/main/java/DemoGit/DemoClass2.java
@@ -0,0 +1,16 @@
+package DemoGit;
+
+public class DemoClass2 {
+	
+	
+	public void Method1() {
+		
+		System.out.println("this for Giit demo");
+	}
+	
+public void Method2() {
+		
+		System.out.println("this for Giit demo");
+	}
+
+}
